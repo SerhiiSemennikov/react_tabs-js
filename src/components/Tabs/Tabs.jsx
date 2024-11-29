ecc198a00154ce848fd1f1c19ecb52703957f1b6
SerhiiSemennikov <semennik@hotmail.com>
2024-11-29 15:33:30 +0100

    copy1

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,29 @@
-export const Tabs = () => {};
+import cn from 'classnames';
+
+export const Tabs = ({ goods, currentTab, set }) => {
+  return (
+    <div className="tabs is-boxed">
+      <ul>
+        {goods.map(good => (
+          <li
+            data-cy="Tab"
+            key={good.id}
+            className={cn`${good.id === currentTab.id && 'is-active'}`}
+          >
+            <a
+              data-cy="TabLink"
+              href={`#${good.id}`}
+              onClick={() => {
+                if (good.id !== currentTab.id) {
+                  set(good.id);
+                }
+              }}
+            >
+              {good.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
